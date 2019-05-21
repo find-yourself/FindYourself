b4ab00b8ddee3a4de143e198df066879c3044033
Sergeygeek <sukhorukov1986@yandex.ru>
2019-05-21 06:50:42 +0400

    DB settings

--- config/db.php
+++ config/db.php
@@ -1,16 +1,27 @@
 <?php
 
-$url =  parse_url(getenv("DATABASE_URL"));
-
-return [
+if ($_SERVER['SERVER_NAME'] == "find-yourself.herokuapp.com") {
+    $url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+    $host = $url["host"];
+    $username = $url["user"];
+    $password = $url["pass"];
+    $dbname = substr($url["path"], 1);
+} else {
+    $host = 'localhost';
+    $dbname = 'yii2basic';
+    $username = 'root';
+    $password = '';
+}
+$dbConfig = [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=yii2basic',
-    'username' => 'root',
-    'password' => '',
+    'dsn' => "mysql:host={$host};dbname={$dbname}",
+    'username' => $username,
+    'password' => $password,
     'charset' => 'utf8',
-
     // Schema cache options (for production environment)
     //'enableSchemaCache' => true,
     //'schemaCacheDuration' => 60,
     //'schemaCache' => 'cache',
 ];
+return $dbConfig;
+
