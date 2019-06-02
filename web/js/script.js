249ebd494904f47f370f966ea57963d835d635f8
NataliaPavlova87 <natalia_pavlova@bk.ru>
2019-06-02 12:56:57 +0300

    Create script.js
    Инициализвция карусели

---
+++ web/js/script.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+
+    $('.owl-carousel').owlCarousel({
+      loop: false,
+      margin: 10,
+      nav: false,
+      items: 1,
+      dots: true,
+  });
+
+
+
+});
+
+
+
+
