092dbd7776d1046fe0b69159ea19f3cb78e5e701
NataliaPavlova87 <natalia_pavlova@bk.ru>
2019-06-02 12:59:18 +0300

    Create start.php
    Главная страница

---
+++ views/site/start.php
@@ -0,0 +1,24 @@
+<?php
+
+/* @var $this yii\web\View */
+
+use yii\helpers\Url;
+use yii\helpers\Html;
+
+$this->title = 'Главная';
+
+?>
+
+<div class="content">
+    <header>
+        <div class="line-grey"></div>
+        <div class="line-dark">
+            <h1><?= Html::encode($this->title) ?></h1>
+        </div>
+    </header>
+    <div class="content-inside">
+        <a href="/site/about-tests/" class="start-link">Тестирование</a>
+        <a href="#" class="start-link">Список профессий</a>
+        <a href="#" class="start-link">Список курсов</a>
+    </div>
+</div>
