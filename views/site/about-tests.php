41eb35298b46b6dc649b906548e96f5578be3276
NataliaPavlova87 <natalia_pavlova@bk.ru>
2019-06-02 12:59:58 +0300

    Update about-tests.php
    Страница типов тестов

--- views/site/about-tests.php
+++ views/site/about-tests.php
@@ -5,25 +5,22 @@
 use yii\helpers\Html;
 Use yii\helpers\Url;
 
-$this->title = 'Виды тестов';
-$this->params['breadcrumbs'][] = $this->title;
-
-var_dump($data);
+$this->title = 'Тесты';
 ?>
 
-<?= Html::a('Назад', Url::home(), ['class' => 'btn btn-primary'])?>
+    <div class="content">
+        <header>
+        <div class="line-grey"></div>
+        <div class="line-dark">
+            <?= Html::a('', Url::to(['start']), ['class' => 'header-link'])?>
+            <h1><?= Html::encode($this->title) ?></h1>
+        </div>
+        </header>
+        <div class="about-inside">
+            <?php foreach ($data as $datum): ?>
 
-<div class="site-about">
-    <h1><?= Html::encode($this->title) ?></h1>
+            <?= Html::a($datum['name'], Url::to(['tests', 'id' => $datum['id']]), ['class' => 'about-link']) ?>
+            <?php endforeach; ?>
+        </div>
+    </div>
 
-    <?php foreach ($data as $datum): ?>
-
-      <?= Html::a($datum['name'], Url::to(['tests', 'id' => $datum['id']]), ['class' => 'btn btn-success']) ?>
-<!--      --><?php //var_dump($datum) ?>
-      <br>
-      <br>
-
-    <?php endforeach; ?>
-
-    <code><?= __FILE__ ?></code>
-</div>
