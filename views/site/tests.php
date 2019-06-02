cfec7e555e898d024b6d48a6f4fd1f809ed449bb
NataliaPavlova87 <natalia_pavlova@bk.ru>
2019-06-02 12:59:10 +0300

    Update tests.php
    Страница тестов

--- views/site/tests.php
+++ views/site/tests.php
@@ -5,19 +5,21 @@ use yii\helpers\Url;
 
 $this->title = $data['name'];
 
-var_dump($data);
 ?>
 
-<?= Html::a('Назад', Url::to(['about-tests']), ['class' => 'btn btn-primary'])?>
 
+<div class="content">
+    <header>
+      <div class="line-grey"></div>
+      <div class="line-dark">
+          <?= Html::a('', Url::to(['about-tests']), ['class' => 'header-link'])?>
+          <h1><?= Html::encode($this->title) ?></h1>
+      </div>
+    </header>
+    <div class="tests-inside">
+        <?php foreach ($data['tests'] as $datum): ?>
+        <?= Html::a($datum['name'], Url::to(['test', 'id' => $datum['id']]), ['class' => 'tests-link'])?>
 
-<div class="site-about">
-  <h1><?= Html::encode($this->title) ?></h1>
-
-  <?php foreach ($data['tests'] as $datum): ?>
-    <?php var_dump($datum) ?>
-    <?= Html::a($datum['name'], Url::to(['test', 'id' => $datum['id']]), ['class' => 'btn btn-success'])?>
-
-  <?php endforeach; ?>
-
-</div>
+      <?php endforeach; ?>
+    </div>
+  </div>
