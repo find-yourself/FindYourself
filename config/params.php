b3ba285ceb4f9970fb4cd31a3810aee2407ddc72
NataliaPavlova87 <natalia_pavlova@bk.ru>
2019-06-06 23:20:02 +0300

    Update params.php

--- config/params.php
+++ config/params.php
@@ -4,5 +4,5 @@ return [
     'adminEmail' => 'admin@example.com',
     'senderEmail' => 'noreply@example.com',
     'senderName' => 'Example.com mailer',
-  'server_url' => getenv("SERVER_URL") ? getenv("SERVER_URL") : 'http://' . getenv('HTTP_HOST'),
+    'server_url' => getenv("SERVER_URL") ? getenv("SERVER_URL") : 'https://find-yourself-stage.herokuapp.com',
 ];
