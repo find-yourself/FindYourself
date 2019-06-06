b84deb9ec2fdea50d8fa37422fbb17067a82bc0c
NataliaPavlova87 <natalia_pavlova@bk.ru>
2019-06-06 23:21:04 +0300

    Update quiz-item.php
    отображение теста Соломина

--- views/site/quiz-item.php
+++ views/site/quiz-item.php
@@ -7,24 +7,35 @@ use yii\helpers\ArrayHelper;
 
 $this->title = $data['name'];
 
-var_dump($data);
+// var_dump($data);
+
 ?>
 
-<?= Html::a('Назад', Url::to(['quiz', 'id' => $data['type_id']]), ['class' => 'btn btn-primary'])?>
-
-<div class="site-about">
-  <h1><?= Html::encode($this->title) ?></h1>
-
-  <?php foreach ($data['questions'] as $datum): ?>
-
-    <?= Html::a($datum['text'], '#', ['class' => 'btn btn-success'])?>
-
-  <?php endforeach; ?>
-
-<!--  --><?php //$form = ActiveForm::begin() ?>
-<!---->
-<!--  --><?//= $form->field($model, 'text[]')->radioList( ArrayHelper::map($questions, 'id', 'text') ); ?>
-<!---->
-<!--  --><?php //ActiveForm::end(); ?>
-
+<div class="content">
+      <header>
+        <div class="line-grey"></div>
+        <div class="line-dark">
+            <?= Html::a('', Url::to(['quiz', 'id' => $data['id']]), ['class' => 'header-link'])?>
+            <h1><?= Html::encode($this->title) ?></h1>
+        </div>
+        </header>
+    <?php foreach ($newData as $key => $value): ?>
+    <div id="block-<?php echo $key; ?>" data-id="<?php echo $key; ?>" class="block-questions">
+      <?php foreach ($value as $val): ?>
+      <div class="block-links">
+          <p class="question" id="q-<?php echo $val['id']; ?>"><?php echo $val['text']; ?></p>
+                  <input class="radio"  id="input1-<?php echo $val['id']; ?>" type="radio" value="1" name="<?php echo $val['text']; ?>">
+                  <label for="input1-<?php echo $val['id']; ?>"></label>
+                  <input class="radio"  id="input2-<?php echo $val['id']; ?>" type="radio" value="2" name="<?php echo $val['text']; ?>">
+                  <label for="input2-<?php echo $val['id']; ?>"></label>
+                  <input class="radio"  id="input3-<?php echo $val['id']; ?>" type="radio" value="3" name="<?php echo $val['text']; ?>">
+                  <label for="input3-<?php echo $val['id']; ?>"></label>
+                  <input class="radio"  id="input4-<?php echo $val['id']; ?>" type="radio" value="4" name="<?php echo $val['text']; ?>">
+                  <label for="input4-<?php echo $val['id']; ?>"></label>
+        </div>
+        <?php endforeach; ?>
+        <button class="answer-link">Далее</button>
+        <button class="result">Узнать результат</button>
+    </div>
+    <?php endforeach; ?>
 </div>
