4337c68bd079d99d8de232bca4d24f71f5b311e6
NataliaPavlova87 <natalia_pavlova@bk.ru>
2019-06-16 19:36:43 +0300

    Update result.php

--- views/site/result.php
+++ views/site/result.php
@@ -2,8 +2,18 @@
 
 
 ?>
-
-<h1>Ваш результат теста</h1>
-<?php foreach ($resultTest as $value): ?>
+<div class="content">
+<header>
+      <div class="line-grey"></div>
+      <div class="line-dark">
+      <h1>Ваш результат теста</h1>
+      </div>
+  </header>
+  <div class="content-pad">
+  <?php foreach ($resultTest as $value): ?>
 <p><?= $value?></p>
 <?php endforeach; ?>
+  </div>  
+
+</div>
+
