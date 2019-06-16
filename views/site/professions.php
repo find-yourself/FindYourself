a765efef32abd33c1884d06481fc36a456a1cb3b
NataliaPavlova87 <natalia_pavlova@bk.ru>
2019-06-16 19:37:01 +0300

    Update professions.php

--- views/site/professions.php
+++ views/site/professions.php
@@ -1,21 +1,20 @@
 <?php
 //use yii\helpers\Url;
 use yii\helpers\Html;
-$this->title = 'Профессии';
+$this->title = 'Список профессий';
 ?>
 
-<div class="content">
+<div class="content content-auto">
     <header>
         <div class="line-grey"></div>
         <div class="line-dark">
+            <a href="/site/start/" class="header-link"></a>
             <h1><?= Html::encode($this->title) ?></h1>
         </div>
     </header>
-    <div class="content-inside">
-        <ul>
+        <ul class="profession">
             <?php foreach ($data_professions as $profession): ?>
                 <li><?php echo $profession['name'] ?></li>
             <?php endforeach; ?>
         </ul>
-    </div>
 </div>
