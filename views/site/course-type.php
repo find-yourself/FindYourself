f3a1e19df7fd8b2a584c494555dcc4e260b18dfb
NataliaPavlova87 <natalia_pavlova@bk.ru>
2019-06-16 19:37:19 +0300

    Update course-type.php

--- views/site/course-type.php
+++ views/site/course-type.php
@@ -12,12 +12,16 @@ Url::remember();
   <header>
     <div class="line-grey"></div>
     <div class="line-dark">
+    <a href="/site/start/" class="header-link"></a>
       <h1><?= Html::encode($this->title) ?></h1>
     </div>
   </header>
-  <div class="content-inside">
+  <div class="content-course">
+    <div class="img">
+    <img src="/img/man.png" alt="">
+    </div>
     <?php foreach ($data as $datum): ?>
-      <?= Html::a($datum['name'], Url::to(['course', 'id' => $datum['id']]), ['class' => 'start-link'])?>
+      <?= Html::a($datum['name'], Url::to(['course', 'id' => $datum['id']]), ['class' => 'start-link-course'])?>
     <?php endforeach; ?>
   </div>
 </div>
